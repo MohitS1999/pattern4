75ac98fce76b66c34f72ef0fd532288d3c1dac0e
Mohit singh <singhmohit9718@gmail.com>
2020-02-02 11:58:00 +0530

    PYTHON

---
+++ pattern4.py
@@ -0,0 +1,15 @@
+#4
+##             *****
+##              ****
+##               ***
+##                ** 
+##                 * 
+def pattern(n):
+    for i in range(n):
+        for j in range(n):
+            if  i-j<=0:
+                print('*',end='')
+            else:
+                print(' ',end='')
+        print()
+pattern(5)
